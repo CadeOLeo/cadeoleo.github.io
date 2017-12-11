a5ccf56d59bace8c237fad18628d53800d799642
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2017-12-11 02:26:29 -0200

    Apply service-worker and manifest.json

---
+++ sw-precache-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    staticFileGlobs: [
+        'browserconfig.xml',
+        '**.html',
+        'manifest.json',
+        'service-worker.js',
+        'assets/images/**.*',
+        'assets/javascripts/cadeoleo.js',
+        'assets/javascripts/main.js',
+        'assets/stylesheets/**.*',
+        'bower_components/bootstrap/dist/css/*',
+        'bower_components/bootstrap/dist/fonts/*',
+        'bower_components/bootstrap/dist/js/*',
+        'bower_components/bootswatch/cerulean/bootstrap.min.css',
+        'bower_components/jumbotron-narrow/index.css',
+        'bower_components/download_webfont/cadeoleo.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/css/bootstrap-datepicker.min.css',
+        'bower_components/flags/flags.css',
+        'bower_components/share-button/share-button.min.css',
+        'bower_components/ie10-viewport-bug-workaround/index.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment-with-locales.min.js',
+        'bower_components/moment-precise-range-object/readable-range.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/locales/bootstrap-datepicker.pt-BR.min.js',
+        'bower_components/jquery-flash-element/index.js',
+        'bower_components/share-button/share-button.js',
+        'bower_components/download_webfont/*',
+        'bower_components/flags/*'
+    ]
+};
